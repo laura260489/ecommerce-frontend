3915a579b0a944b50709d264e8f9474400e824f1
laura260489 <lauradiezmil@hotmail.com>
2025-08-02 13:42:04 -0500

    page mi account

--- projects/user-portal/src/app/containers/user-profile/user-profile.module.ts
+++ projects/user-portal/src/app/containers/user-profile/user-profile.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile.component';
 import { RouterModule } from '@angular/router';
-
+import {CardModule} from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 @NgModule({
   imports: [
     CommonModule,
@@ -12,7 +16,12 @@ import { RouterModule } from '@angular/router';
         component: UserProfileComponent
       },
     ]),
+    CardModule,
+    ButtonModule,
+    HttpClientModule,
+    DynamicDialogModule,
   ],
-  declarations: [UserProfileComponent]
+  declarations: [UserProfileComponent],
+  providers: [DialogService]
 })
 export class UserProfileModule { }
