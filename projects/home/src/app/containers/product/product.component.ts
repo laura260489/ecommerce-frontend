f82edc6830e1fcd060191a93c5866a3ac7230eca
laura260489 <lauradiezmil@hotmail.com>
2025-08-02 17:06:03 -0500

    development of product page

--- projects/home/src/app/containers/product/product.component.ts
+++ projects/home/src/app/containers/product/product.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  public producto: any = null;
+  public data: any[] = [];
 
-  ngOnInit() {
+  public quantities: { label: string; value: number }[] = [];
+  public quantity: number = 1;
+  public stock: number = 0;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.http.get<any>(`https://dummyjson.com/products/${id}`).subscribe({
+        next: (product) => {
+          this.producto = product;
+          this.data = Object.entries(product)
+            .filter(([key]) => ['brand', 'weight', 'warrantyInformation'].includes(key))
+            .map(([key, value]) => ({ campo: key, valor: value }));
+          this.stock = product.stock;
+          const maxQuantity = this.stock >= 6 ? 6 : this.stock;
+
+          this.quantities = Array.from({ length: maxQuantity }, (_, i) => {
+            const number = i + 1;
+            return {
+              label: `${number} ${number === 1 ? 'unidad' : 'unidades'}`,
+              value: number
+            };
+          });
+        },
+        error: () => {
+
+        }
+      });
+    }
+  }
+
+  onQuantityChange(value: number): void {
+    this.quantity = value;
+  }
+
+  get inStock(): boolean {
+    return this.producto?.availabilityStatus?.includes('In Stock');
   }
 
 }
--- projects/home/src/app/containers/product/product.module.ts
+++ projects/home/src/app/containers/product/product.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { GalleriaModule } from 'primeng/galleria';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -10,6 +16,12 @@ import { RouterModule } from '@angular/router';
       path: ':id',
       component: ProductComponent
     }]),
+    HttpClientModule,
+    GalleriaModule,
+    ButtonModule,
+    DividerModule,
+    DropdownModule,
+    FormsModule
   ],
   declarations: [ProductComponent],
 })
