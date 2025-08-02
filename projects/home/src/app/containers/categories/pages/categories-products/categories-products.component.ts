fff60d762afd59b34432e8329bd873d9e48e6fe2
laura260489 <lauradiezmil@hotmail.com>
2025-08-02 14:56:20 -0500

    page of categories and products

---
+++ projects/home/src/app/containers/categories/pages/categories-products/categories-products.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-categories-products',
+  templateUrl: './categories-products.component.html',
+  styleUrls: ['./categories-products.component.scss']
+})
+export class CategoriesProductsComponent implements OnInit {
+
+  slug!: string;
+  categoryProducts: any;
+  first = 0;
+  rows = 10;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router,) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.slug = params.get('slug');
+      this.http.get<any[]>('https://dummyjson.com/products/category/'+ this.slug).subscribe({
+        next: (data) => {
+          this.categoryProducts = data;
+        },
+        error: () => {
+          
+        }
+      });
+      console.log(params.get('slug'))
+    });
+  }
+
+  onPageChange(event: any) {
+    this.first = event.first;
+    this.rows = event.rows;
+  }
+
+  goToProduct(productId: number) {
+    this.router.navigate(['/producto', productId]);
+  }
+
+}
--- projects/home/src/app/containers/product/product.module.ts
+++ projects/home/src/app/containers/product/product.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule.forChild([{
-      path: '',
+      path: ':id',
       component: ProductComponent
     }]),
   ],
